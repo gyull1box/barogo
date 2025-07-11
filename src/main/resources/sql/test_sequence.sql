00a2ac5fdef6b75f6113cd7ab132f42ddd76805d
gyull1box <thgml4046@hanmail.net>
2025-07-12 08:49:19 +0900

    test ddl script commit

---
+++ src/main/resources/sql/test_sequence.sql
@@ -0,0 +1,28 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 토요일-7월-12-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence DRIVER_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEST_USER"."DRIVER_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence DSPCH_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEST_USER"."DSPCH_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence ORDER_DTL_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEST_USER"."ORDER_DTL_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence ORDER_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEST_USER"."ORDER_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PAYMENT_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEST_USER"."PAYMENT_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
